f7529900ec67a58529b1553f3bdb42db57dc2001
Luis <proyector27@gmail.com>
2025-11-29 19:08:41 -0400

    Update Firebase App ID and config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,10 +6,11 @@ import { getFunctions } from "firebase/functions";
 const firebaseConfig = {
   apiKey: "AIzaSyCZKE9ZRLhNJGxf-PNdbR6IjgMCl5xvkbA",
   authDomain: "hecho-srl-free.firebaseapp.com",
+  databaseURL: "https://hecho-srl-free.firebaseio.com",
   projectId: "hecho-srl-free",
   storageBucket: "hecho-srl-free.firebasestorage.app",
   messagingSenderId: "216623683956",
-  appId: "1:216623683956:web:7b7de0220203978c6db421",
+  appId: "1:63203392566:web:e685fc33618e273225ec97",
   measurementId: "G-XHS77PK8HC"
 };
 
