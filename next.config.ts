1c7e7fd348e26a5c69e5300cbb2e401454b14c8c
Luis <proyector27@gmail.com>
2026-05-05 18:12:20 -0400

    fix: configurar distDir dinamico para Vercel

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const withPWA = withPWAInit({
 });
 
 const nextConfig: NextConfig = {
-  distDir: '.next2',
+  distDir: process.env.VERCEL === "1" ? ".next" : ".next2",
   images: {
     remotePatterns: [
       {
